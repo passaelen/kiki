53a5978ae3275ac32c80e078017fbd6245e2e4f7
passaelen <pas.saelen@gmail.com>
2026-05-02 16:35:39 +0200

    Update achats.js

--- achats.js
+++ achats.js
@@ -1,8 +1,8 @@
 const achats = [
- {date: "20/03/2026", type: "ETF_SENIOR", montant: 0.0, parts: 0, label: "vide"} 
+{date: "20/03/2026", type: "ETF_SENIOR", montant: 0.0, parts: 0, label: "vide"} 
  ]
 
 
 const metaux = [
-{date: "15/03/2025", type: "Or", loc: "Bel.", tot: 2988.05, poids: 31.10, label: "P. 10 × 3,10g."}
+{date: "15/03/2025", type: "OR", loc: "Bel.", tot: 2988.05, poids: 31.10, label: "⅒ Koala"}
  ]; 
