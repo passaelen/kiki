fdba5f9ce1ed34d8492ecf6810c8216f777f7940
passaelen <pas.saelen@gmail.com>
2026-05-02 14:57:50 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,15 @@
+const DATA = {
+  portfolio: {
+    or_suisse_g: 0.0,
+    or_belgique_g: 31.10,
+    argent_g: 0.0,
+
+    or_suisse_cost: 0.0,
+    or_belgique_cost: 2988.05,
+    argent_cost: 0.0
+  }
+
+  
+};
+
+// ⚠️ IMPORTANT
