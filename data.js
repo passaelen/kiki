cf3000c5f92c60e69167e897c7ee489e43e87909
passaelen <pas.saelen@gmail.com>
2026-05-02 15:19:36 +0200

    Update data.js

--- data.js
+++ data.js
@@ -7,9 +7,21 @@ const DATA = {
     or_suisse_cost: 0.0,
     or_belgique_cost: 2988.05,
     argent_cost: 0.0
+  },
+
+  etf: {
+    monde: {
+      parts: 0,
+      buy: 0
+    },
+    seniors: {
+      parts: 0,
+      buy: 0
+    },
+    juniors: {
+      parts: 0,
+      buy: 0
+    }
   }
-
-  
 };
-
 // ⚠️ IMPORTANT
