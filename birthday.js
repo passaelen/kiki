73c831a70d6b10bb7673748ca51fb3883cc385a5
passaelen <pas.saelen@gmail.com>
2026-05-03 14:07:03 +0200

    Update birthday.js

--- birthday.js
+++ birthday.js
@@ -85,7 +85,7 @@ function checkBirthday(config){
 
   // 🎂 MESSAGE
   const msg = document.createElement("div");
-  msg.innerText = "🎂 Joyeux anniversaire Kiki 😘😘😘!";
+  msg.innerText = "🎂 Joyeux anniversaire Kiki 😘😘😘 !";
   msg.style.position = "fixed";
   msg.style.top = "20px";
   msg.style.left = "50%";
@@ -107,6 +107,6 @@ function checkBirthday(config){
 
 // 🎂 CONFIG
 checkBirthday({
-  day: 3,
-  month: 5
+  day: 5,
+  month: 2
 });
